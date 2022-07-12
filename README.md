45795fe85392dd3098b5e62ff52d51812d89396e
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-12 20:29:25 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# load-balancer
+ a simple load balancer using round robin algorithm
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http/httputil"
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+func main() {
+
+}
