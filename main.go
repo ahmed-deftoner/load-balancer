9b605dfe7e2fa4b164ff9d914083d9f9a1ebb4f5
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-12 20:40:45 +0500

    simple server func

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
-import "net/http/httputil"
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
 
 type simpleServer struct {
 	addr  string
 	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println("error : %v", err)
+		os.Exit(1)
+	}
 }
 
 func main() {
